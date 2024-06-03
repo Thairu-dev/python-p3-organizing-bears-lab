78f87ebe361411b215241bbb19ff846c1a0061e1
Joseph_Thairu <joejoement@gmail.com>
2024-06-03 16:38:16 +0300

    complted

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ("Mr. Chocolate", 3, "M", "Black", "social", 1),
+    ("Rowdy", 8, "M", "Black", "optimistic", 1),
+    ("Tabith", 2, "F", "Brown", "social", 1),
+    ("Seagent Brown", 10, "M", "white", "social", 1),
+    ("Melissa", 5, "F", "white", "goofy", 1),
+    ("Grinch", 7, "M", "Black", "Agressive", 0),
+    ("Wendy", 6, "F", "white", "shy", 1),
+    (null, 20, "M", "black", "aggressive", 0);
+    
+
+ 
